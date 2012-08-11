84a17490bf0a8089745c144bc8144c679b1ddd2c
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- XoopsModules/sam/releases/1.02/modules/sam/language/english/blocks.php
+++ XoopsModules/sam/releases/1.02/modules/sam/language/english/blocks.php
@@ -1,9 +1,9 @@
 <?php
 
 	// options
-	define('_BL_SAM_ADDTHIS_USERNAME', 'Addthis Username:&nbsp;');
+	define('_BL_SAM_ADDTHIS_USERNAME','Addthis Username:&nbsp;');
 
 	// Addthis Javascript
-	define('_BL_SAM_ADDTHIS_JAVASCRIPT', 'http://s7.addthis.com/js/250/addthis_widget.js#username=%s');
+	define('_BL_SAM_ADDTHIS_JAVASCRIPT','http://s7.addthis.com/js/250/addthis_widget.js#username=%s');
 	
 ?>
